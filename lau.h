dcbf26e63adcff9993adbed0d9a5893e80bfd71c
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2022-12-10 21:44:27 +0900

    added additional strings

--- lau.h
+++ lau.h
@@ -1,10 +1,39 @@
-/**
+/*
 */
 
 #include <string.h>
 #include <iostream>
 
 std::string const lau_strings[] = {
+	//Normal
 	"PPPK",
 	"PPP2+K",
-	"PPP7+K"};
+	"PPP7+K",
+	"P4+PP",
+	"4+PKP",
+	"44+PK+G",
+	"2_3+PP+K",
+	"3346+P",
+	"3+PPPK",
+	"3+PPP2+K",
+	"3+PPP7+K",
+	"3+KPP",
+	"9+K",
+	"7+K",
+	"3+P+K",
+	"6+K+G",
+	"2+K+G",
+	"9+K+GP",
+
+	//Kokei stance
+	"K+G 55PPP",
+	"K+G 5 K",
+	"K+G 5 K+GK",
+
+	//Jumping
+	"9+G5K",
+
+	//Backturned
+	"44+P 5 2+K",
+	"44+P 5 8+K+G"
+};
