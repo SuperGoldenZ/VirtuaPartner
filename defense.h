54a8955409b1940038f1a4f98f4980a50e997f23
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2022-12-11 13:27:48 +0900

    adding defense and jeff

---
+++ defense.h
@@ -0,0 +1,51 @@
+#pragma once
+/*
+*/
+
+#include <string.h>
+#include <iostream>
+
+std::string const struggle_strings[] = {
+	
+
+	//Stick only
+	"46",
+	"4565",
+
+	//Buttons Only
+	"PK",
+	"P+K",
+	"P+K5",
+
+	//Alternating P and K
+	"4+P6+K",	
+	"8+P2+K",
+	"4+P7+K",
+	"7+P9+K",
+	"7+P1+K",
+
+	//Alternating P and K and five
+	"4+P56+K5",
+	"4+P56+P5",
+    "8+P52+K5",
+	"4+P57+K5",
+	"7+P59+K5",
+	"7+P51+K5",
+
+	//Boring
+	"4+P54+P5",
+	"4+P545",
+	"4+P5454+P5",
+
+	//P only
+	"4+P6+P",
+	"4+P7+P",
+	"7+P9+P",
+	"7+P1+P",
+
+	//K Only
+	"4+K6+K",
+
+	//P and K togeter
+	"4+P+K6+P+K",
+};
