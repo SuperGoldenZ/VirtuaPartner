1080973eaca676183bbf287295e24c6a4f836bea
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2022-12-12 20:24:29 +0900

    adding akira moves

---
+++ akira.h
@@ -0,0 +1,18 @@
+/*
+*/
+
+#include <string.h>
+#include <iostream>
+
+std::string const akira_strings[] = {
+	//Normal
+	"K+G!",
+	"66+KK",
+	"2+K",
+	"466+P+K",
+	"3346+P",
+	"33+P",
+	"6+P45K",
+	"K+GP",	
+	"2+P+K+G"
+};
