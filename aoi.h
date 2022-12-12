292df9435f63dfe56ae22ce2afdd9b24daac4ceb
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2022-12-12 16:50:21 +0900

    added new moves

---
+++ aoi.h
@@ -0,0 +1,29 @@
+/*
+*/
+
+#include <string.h>
+#include <iostream>
+
+std::string const aoi_strings[] = {
+	//Normal
+	"PP8+P5P_",
+	"PP2+P5P_",
+    "3+P+K5P_",
+
+	"PP2+P5P5",
+	"2_3+P",
+	"6+KP",
+	"6+KK",
+	"66+K",
+	"1+K",
+	"6+P+KP5",
+	"64+P+KP5",
+	"44+P+K5",
+
+	"2+K+G",
+	"9+G5K",
+
+	//Tenchi In'yo	
+	"P+K+G_6K",
+	"P+K+G_	4K+G"
+};
