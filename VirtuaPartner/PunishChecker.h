cebd941231d1a669c84d8dd385d9d3e6fc2b1de4
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2023-01-03 01:35:07 +0900

    removed unnecessary functions

--- VirtuaPartner/PunishChecker.h
+++ VirtuaPartner/PunishChecker.h
@@ -1,6 +1,9 @@
 #ifndef PUNISH_CHECKER
 #define PUNISH_CHECKER
 
+#include <windows.h>
+#include <string>
+
 #pragma once
 class PunishChecker
 {
@@ -8,23 +11,28 @@ public:
 	enum class AdvantageClass { NONE, THROW, PUNCH, ELBOW, KNEE };
 	AdvantageClass advantageClass;
 
+	bool checkPoint(int x, int y, int r, int g, int b);
+	static bool checkPoint(HWND virtuaFighterWindow, int x, int y, int r, int g, int b);
 	virtual byte giveFeedback() = 0 {};
 	static void playSuccessSound();
-	static void playFailureSound();	
+	static void playFailureSound();
+	static std::string getSelectedPlayer1(HWND virtuaFighterWindow);
 
 protected:
 	const int WHITE_R = 255;
 	const int WHITE_G = 251;
 	const int WHITE_B = 255;
-	void getAdvantageAmount();
-	int frameAdvantage;
 	HDC dc;
+	HWND _virtuaFighterWindow;
+
+	int frameAdvantage;
 	bool maxPunishment = false;
 	bool guaranteedDamage = true;
 	bool cpuKnockdown = false;
-
 	bool recoversLow;
-	bool hitsLow;	
+	bool hitsLow;
+
+	void getAdvantageAmount();
 };
 
 #endif
